c7a7803dd7968d924966536f5b857cbea7c994ab
Georgina5-2 <georgina.joeanand@gmail.com>
2023-01-17 13:02:35 -0500

    <your message here>

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,21 @@
+var topics="Git";
+if(topics==="HTML")
+{
+    console.log("Let's study HTML");
+}
+else if(topics==="CSS")
+{
+    console.log("Let's study CSS");
+}
+else if(topics==="Git")
+{
+    console.log("Let's study Git");
+}
+else if(topics==="Javascript")
+{
+    console.log("Let's study Javascript");
+}
+else
+{
+    console.log("Please try again");
+}
